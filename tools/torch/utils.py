ba7c5f7440d0dcfefc8ae9dbbcc2f5ca083ac02b
jsyoo61 <jsyoo61@naver.com>
2021-03-31 02:26:20 +0900

    added spread_device() in torch.utils

--- tools/torch/utils.py
+++ tools/torch/utils.py
@@ -1,3 +1,12 @@
+import torch
 
 def get_device(model):
     return next(model.parameters()).device
+
+def spread_device(job_num = None):
+    if job_num != None:
+        gpu_id = job_num % torch.cuda.device_count()
+    else:
+        gpu_id = torch.cuda.default_stream().device.index
+    device = torch.device(f"cuda:{gpu_id}" if torch.cuda.is_available() else "cpu")
+    return device
