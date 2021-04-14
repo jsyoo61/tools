c7d4d9cc8d63324ff3c75f0c012294504bf5b8af
jsyoo61 <jsyoo61@naver.com>
2021-04-14 10:21:35 +0900

    Added "isfile" argument in os.listdir

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "2021.04.12.4"
+__version__ = "2021.04.14.0"
 # from . import tools, data, my_random
 from .tools import *
 from . import my_random, os
--- tools/os.py
+++ tools/os.py
@@ -1,10 +1,59 @@
-import os as os
+import os
 
 __all__ = \
 ['listdir']
 
-def listdir(path, isdir=True):
-    if isdir:
-        return [dir for dir in os.listdir(path) if os.path.isdir(os.path.join(path, dir))]
+def makedirs(path, exist_ok=True):
+    os.makedirs(path, exist_ok=exist_ok)
+
+def listdir(path, isdir=False, isfile=False, join=False):
+    '''
+    :func listdir:
+
+    :param path:
+    :param isdir:
+    :param isfile:
+    to get files what start with ".", set isfile=''.
+    :param join:
+    '''
+    # Safety check
+    if type(isfile)==str:
+        ext = isfile # extension
+        isfile_str = True
+        isfile = True
     else:
-        return os.listdir(path)
+        isfile_str=False
+    assert not (isdir and isfile), 'only one of argument "isdir" and "isfile" can be True'
+
+    dir_list = os.listdir(path)
+    dir_list_joined = [os.path.join(path, dir) for dir in dir_list]
+
+    if join:
+        dir_list = dir_list_joined
+    # else: pass
+
+    if isdir:
+        return [dir for dir, dir_joined in zip(dir_list, dir_list_joined) if os.path.isdir(dir_joined)]
+    elif isfile:
+        if isfile_str:
+            return [dir for dir, dir_joined in zip(dir_list, dir_list_joined) if os.path.isfile(dir_joined) and (os.path.splitext(dir)[1] == ext)]
+        else:
+            return [dir for dir, dir_joined in zip(dir_list, dir_list_joined) if os.path.isfile(dir_joined)]
+    else:
+        return dir_list
+
+
+# if __name__ == '__main__':
+#     listdir('torch', isdir=False, join=False)
+#     listdir('torch', isdir=False, join=True)
+#     listdir('torch', isdir=True, join=False)
+#     listdir('torch', isdir=True, join=True)
+#
+#     listdir('torch', isfile=True, join=False)
+#     listdir('torch', isfile=True, join=True)
+#     listdir('torch', isfile='.p', join=False)
+#     listdir('torch', isfile='.py', join=True)
+#     listdir('torch', isfile='', join=True)
+#
+#     listdir('torch', isdir=True, isfile=True)
+#     listdir('torch', isdir=True, isfile='py')
